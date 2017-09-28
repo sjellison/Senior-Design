63261cdf77e25bfb547214c82ab82d873420f3e4
Alex Orman <aorman@iastate.edu>
2017-09-28 15:39:08 -0500

    Started working on shell of file

---
+++ NeuralNetwork/NNModule.py
@@ -0,0 +1,23 @@
+# Pseudo code
+#
+# NN module
+#
+# Camera module will instantiate a NN module.
+# Camera then will pass an image to the instantiated class.
+# Ie:
+# NN = new NNModule()
+#
+# while(true):
+#     img = getImage()
+#     NN.anaylyze(img)
+#     wait 1 second
+
+
+# When we are called, we will do the following
+
+#Constructor:
+#   Initialize the NN, set up any variables
+
+
+#def analyze(img):
+#   This runs the analysis code from example
