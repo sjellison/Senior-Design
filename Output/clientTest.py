548c01397706eb4938b8bdb3b71c3ae161cfd48c
Sean Jellison <jellison@iastate.edu>
2017-10-12 19:03:31 -0500

    Added Output to my branch

---
+++ Output/clientTest.py
@@ -0,0 +1,24 @@
+'''
+Created on Oct 8, 2017
+
+@author: bebop
+'''
+
+import socket, pickle
+
+host = "tx1-dec1710.student.iastate.edu"
+print(host)
+port = 4500
+if __name__ == '__main__':
+    try:
+            
+        client = socket.socket()
+        client.connect((host, port))
+        while(True):
+            print("Getting data")
+            print(pickle.loads(client.recv(72)))
+
+    finally:
+        print("Closing Client")
+        client.close()    
+    pass
---
+++ Output/outputtest.py
@@ -0,0 +1,27 @@
+'''
+Created on Oct 8, 2017
+
+@author: bebop
+'''
+
+import Output as output
+
+if __name__ == '__main__':
+    
+    try:
+        output.init()
+        
+        
+        for i in range(100):
+            name = "name"+str(i)
+            dat1 = int(i *2.5)
+            dat2 = int(i *3.14)
+            dat3 = int((dat1+dat2)/3)
+            data =[name, 'xposition', dat1, 'yposition', dat2, 'distance', dat3]
+        
+            output.out(data)
+        
+    finally:    
+        output.close()
+    
+    pass
