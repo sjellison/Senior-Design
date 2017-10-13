f4f626e4c54fcbbea774834c3cad64f735b2ba0c
Sean Jellison <jellison@iastate.edu>
2017-10-12 19:23:53 -0500

    Playing with things

---
+++ Output/Output.py
@@ -0,0 +1,80 @@
+'''
+Created on Oct 8, 2017
+
+@author: bebop
+'''
+
+import socket, pickle
+import atexit
+
+#serv_addr = './ap_unix_socket' #for unix socket, but that doesn't seem to be supported anymore
+host = socket.gethostname()
+port = 4500
+
+server = socket.socket()
+conn = socket.socket()
+
+def init():
+    global conn
+    global server
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(1)
+    
+    print("Server waiting for client...")
+    conn, client_addr = server.accept()  #returns a connection and an address to the client that connected
+    print("Connected to ", client_addr)
+    #print("Test completed, closing socket")
+    #server.close()
+
+def out(data={}):
+    global conn
+    dataArr = parse(data)
+    print("Sending data...")
+    print("Sending ", dataArr.__sizeof__(), "bytes")
+    if(not(conn.send(pickle.dumps(dataArr)))):
+        print("Errror during server send")
+
+def close():
+    global conn
+    global server
+    conn.close()
+    server.close()
+            
+'''
+This is a very strict parsing of the data. This is being parsed
+with strict knowledge of the data input and that is a bad way
+to do it. This needs to change
+'''      
+def parse(data={}):
+    dat = 0
+    arrPos = 0
+    ret = []
+    for _ in data:
+        if(_ == 'xposition'):
+            dat = 1
+        elif(_ == 'yposition'):
+            dat = 2
+        elif(_ == 'distance'):
+            dat = 3
+            
+        elif(dat == 1):
+            ret.append(_)
+            arrPos += 1
+        elif(dat == 2):
+            ret.append(_)
+            arrPos += 1
+        elif(dat == 3):
+            ret.append(_)
+            arrPos += 1
+            dat = 0
+            
+        else:
+            ret.append(_)
+            arrPos += 1
+        
+    return ret
+        
+        
+        
+        
