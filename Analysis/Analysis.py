2839ccbc06c6f831fe3a16558d37357ee312da0e
Sean Jellison <jellison@iastate.edu>
2017-10-13 18:27:27 -0500

    Typo fixed

--- Analysis/Analysis.py
+++ Analysis/Analysis.py
@@ -78,7 +78,7 @@ Determines the distance to the object
 ''' 
 def getDistance(name, width):
     
-    query = "SELECT width FROM Objects WHERE name=%s" % (name)
+    query = "SELECT width FROM Objects WHERE name=%s;" % (name)
     if(debug):
         print("Query used: " + query)
 
