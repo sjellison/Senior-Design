bba9af336aca32eb7203bdfe268f30c1c1c8da7c
Sean Jellison <jellison@iastate.edu>
2017-10-13 18:09:52 -0500

    Changed object names in the Network.py to better test the Analysis module

--- Network/Network.py
+++ Network/Network.py
@@ -11,7 +11,7 @@ def getList(frame):
     
     for i in range(10):
 #        print("Network: " + str(i))
-        name = "Ob" + str(i)
+        name = "airport"
 #        print("Network: " + name)
         xpos = random.randint(0, 255)
 #        print("Network: " + str(xpos))
