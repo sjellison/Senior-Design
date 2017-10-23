54fc8822b13f2be556d858701161c0a9374c5099
Sean Jellison <jellison@iastate.edu>
2017-10-22 22:38:27 -0500

    Debugging

--- Camera/Camera.py
+++ Camera/Camera.py
@@ -20,7 +20,7 @@ def get_image_old():
 def get_image():
     global camera
     retval, im = camera.read()
-    return cv2.EncodeImage('.jpg', im)
+    return cv2.imencode('.jpg', im)
     
 #convert to grayScale
 def convert(im):
